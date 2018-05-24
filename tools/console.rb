d6fdde3d0736045050734670f77abc06a2c2ded1
prmoreira23 <prm.gredes@gmail.com>
2018-05-23 22:23:47 -0400

    Finished Lyft models.

--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,17 @@ def reload
   load 'config/environment.rb'
 end
 
+p = Passenger.new
+p2 = Passenger.new
+
+d = Driver.new
+d2 = Driver.new
+d3 = Driver.new
+d4 = Driver.new
+
+r = Ride.new(100, d, p)
+r2 = Ride.new(500, d2, p)
+r3 = Ride.new(56, d, p2)
+r4 = Ride.new(48, d4, p)
+
 Pry.start
